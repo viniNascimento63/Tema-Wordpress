59873b151f7146729b813246db972a15acb08c23
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-17 09:48:40 -0300

    incluindo wp_head()

--- wp-content/themes/wp-devs/header.php
+++ wp-content/themes/wp-devs/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <?php wp_head();?>
 </head>
 <body <?php body_class();?> >
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit porro vitae ipsum, odit vel cumque nam minus dolores quas culpa esse quisquam totam mollitia? Eaque culpa nobis soluta corporis aut.</p>
