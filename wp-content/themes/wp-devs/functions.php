6ddd890ac03c8ada02ed4a32d4a922404b0812ef
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-17 10:03:31 -0300

    Organizando comentários

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -1,6 +1,9 @@
 <?php
 
 function wpdevs_load_scripts() {
+
+    /* --- CARREGANDO FOLHAS DE ESTILOS --- */
+
     /*
      * 1° arg - nome do arquivo css
      * 2° arg - caminho da folha de estilo
@@ -13,6 +16,10 @@ function wpdevs_load_scripts() {
     // Usar a função filemtime() somente durante o desenvolvimento.
 
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null);
+
+    /* --- CARREGANDO SCRIPTS --- */
+
+    wp_enqueue_script( 'dropdown', get_template_directory_uri().'/js/dropdown.js', array(), '1.0', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
