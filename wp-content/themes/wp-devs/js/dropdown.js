f605692bb386dff01a8fc2fd75e9eaad3d4c1921
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-19 11:35:08 -0300

    Menu hamburguer

---
+++ wp-content/themes/wp-devs/js/dropdown.js
@@ -0,0 +1,13 @@
+let dropdown = document.querySelector('.menu'), //ul
+submenu = document.querySelector('.sub-menu'), // ul li ul
+buttonClick = document.querySelector('.check-button'), // button
+hamburger = document.querySelector('.menu-icon');
+
+buttonClick.addEventListener('click', () => {
+    dropdown.classList.toggle( 'show-dropdown' );
+    if (submenu) {
+        submenu.classList.toggle( 'show-dropdown' );
+    }
+    hamburger.classList.toggle( 'animate-button' );
+
+});
