85afcd2c0e18696eaff3844e39870ee4dbd46cdb
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-19 11:34:53 -0300

    Retirando algumas seções

--- wp-content/themes/wp-devs/index.php
+++ wp-content/themes/wp-devs/index.php
@@ -3,15 +3,32 @@
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
-                    <section class="hero">
-                        Hero
-                    </section>
-                    <section class="services">
-                        Services
-                    </section>
-                    <section class="home-blog">
-                        Blog
-                    </section>
+                    <h1>Blog</h1>
+                    <div class="container">
+                        <div class="blog-items">
+                            <?php
+                                if (have_posts()):
+                                    while(have_posts()) : the_post();
+                                    ?>
+                                        <article>
+                                            <h2><?php the_title();?></h2>
+                                            <div class="meta-info">
+                                                <p>Posted in <?php echo get_the_date();?> by <?php echo the_author_posts_link();?>.</p>
+                                                <p>Categories: <?php the_category(' ');?></p>
+                                                <p>Tags: <?php the_tags(' ', ', ');?></p>
+                                                <?php the_content();?>
+                                            </div>
+                                        </article>
+                                    <?php
+                                    endwhile;
+                                else:
+                                    ?>
+                                    <p>Nothing yet to be displayed!</p>
+                                <?php endif;
+                            ?>
+                        </div>
+                    </div>
+                    
                 </main>
             </div>
         </div>
