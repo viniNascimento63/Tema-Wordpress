c5aae350b11a5dcd1fa6dada5e753dccb7964bd5
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-22 11:42:58 -0300

    Imagens no header

--- wp-content/themes/wp-devs/general-template.php
+++ wp-content/themes/wp-devs/general-template.php
@@ -5,6 +5,8 @@ Template name: General Template
 ?>
 <?php get_header();?>
 
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height;?>" width="<?php echo get_custom_header()->width?>" alt="">
+
 <div id="content" class="site-content">
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
