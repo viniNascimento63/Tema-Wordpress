90ca554832815da8fe1a9794c5b0a5d5a5b776ae
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-22 10:18:33 -0300

    Create page.php

---
+++ wp-content/themes/wp-devs/page.php
@@ -0,0 +1,26 @@
+<?php get_header();?>
+
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <div class="container">
+                        <div class="page-item">
+                            <?php
+                                while(have_posts()) : the_post();
+                                ?>
+                                    <article>
+                                        <header>
+                                            <h1><?php the_title();?></h1>
+                                        </header>
+                                    </article>
+                                <?php
+                                endwhile;
+                            ?>
+                        </div>
+                    </div>
+                    
+                </main>
+            </div>
+        </div>
+
+<?php get_footer();?>
