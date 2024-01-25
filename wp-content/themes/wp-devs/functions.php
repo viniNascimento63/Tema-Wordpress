d55fe20814225365a0df00207075d8a148976a32
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-25 10:21:41 -0300

    posts na home-page

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -54,3 +54,52 @@ function wpdevs_config() {
 }
 
 add_action( 'after_setup_theme', 'wpdevs_config', 0 );
+
+/* --- SIDEBARS (ÁREA DE WIDGETS) --- */
+add_action( 'widgets_init', 'wpdevs_sidebars');
+function wpdevs_sidebars() {
+    register_sidebar(
+        array(
+            'name'=>'Blog Sidebar',
+            'id'=>'sidebar-blog',
+            'description'=>'This is the Blog Sidebar. You can add your widgets here.',
+            'before_widget'=>'<div class="widget-wrapper">',
+            'after_widget'=>'</div>',
+            'before_title'=>'<h4 class="widget-title">',
+            'after_title'=>'</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name'=>'Service 1',
+            'id'=>'services-1',
+            'description'=>'First Service Area.',
+            'before_widget'=>'<div class="widget-wrapper">',
+            'after_widget'=>'</div>',
+            'before_title'=>'<h4 class="widget-title">',
+            'after_title'=>'</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name'=>'Service 2',
+            'id'=>'services-2',
+            'description'=>'Second Service Area.',
+            'before_widget'=>'<div class="widget-wrapper">',
+            'after_widget'=>'</div>',
+            'before_title'=>'<h4 class="widget-title">',
+            'after_title'=>'</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name'=>'Service 3',
+            'id'=>'services-3',
+            'description'=>'Third Service Area.',
+            'before_widget'=>'<div class="widget-wrapper">',
+            'after_widget'=>'</div>',
+            'before_title'=>'<h4 class="widget-title">',
+            'after_title'=>'</h4>'
+        )
+    );
+}
