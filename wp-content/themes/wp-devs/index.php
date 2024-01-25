4e4b2af905faf6ce08419f230c6c8d3bd98bfefc
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-25 08:29:13 -0300

    Update index.php

--- wp-content/themes/wp-devs/index.php
+++ wp-content/themes/wp-devs/index.php
@@ -17,8 +17,8 @@
                                             <?php the_post_thumbnail(array(200, ''));?>
                                             <div class="meta-info">
                                                 <p>Posted in <?php echo get_the_date();?> by <?php echo the_author_posts_link();?>.</p>
-                                                <p>Categories: <?php the_category(' ');?></p>
-                                                <p>Tags: <?php the_tags(' ', ', ');?></p>
+                                                <p>Categories: <?php the_category( ' ' );?></p>
+                                                <p>Tags: <?php the_tags( ' ', ', ' );?></p>
                                                 <?php the_content();?>
                                             </div>
                                         </article>
