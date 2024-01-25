cf770298df87528adde56698ab0914b397435a48
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-25 10:47:07 -0300

    Template do post em si

--- wp-content/themes/wp-devs/index.php
+++ wp-content/themes/wp-devs/index.php
@@ -13,8 +13,8 @@
                                     while(have_posts()) : the_post();
                                     ?>
                                         <article>
-                                            <h2><?php the_title();?></h2>
-                                            <?php the_post_thumbnail(array(200, ''));?>
+                                            <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+                                            <a href="<?php the_permalink();?>"><?php the_post_thumbnail(array(200, ''));?></a>
                                             <div class="meta-info">
                                                 <p>Posted in <?php echo get_the_date();?> by <?php echo the_author_posts_link();?>.</p>
                                                 <p>Categories: <?php the_category( ' ' );?></p>
