294f39f38798449c099b4f63bae6ef66565e8c2c
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-26 09:43:40 -0300

    Habilitando comentários

--- wp-content/themes/wp-devs/page.php
+++ wp-content/themes/wp-devs/page.php
@@ -1,28 +1,34 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height;?>" width="<?php echo get_custom_header()->width?>" alt="">
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
 
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
                     <div class="container">
                         <div class="page-item">
-                            <?php
-                                while(have_posts()) : the_post();
+                            <?php 
+                                while( have_posts() ) : the_post();
                                 ?>
                                     <article>
                                         <header>
-                                            <h1><?php the_title();?></h1>
+                                            <h1><?php the_title(); ?></h1>
                                         </header>
+                                        <?php the_content(); ?>
                                     </article>
                                 <?php
+
+                                // Verifica se o post atual está aberto para comentários
+                                // E/OU a quantidade de comentários do post
+                                if ( comments_open() || get_comments_number() ) {
+                                    // Carrega a template para comentários
+                                    comments_template();
+                                }
                                 endwhile;
-                            ?>
+                            ?>                                
                         </div>
                     </div>
-                    
                 </main>
             </div>
         </div>
-
-<?php get_footer();?>
+<?php get_footer(); ?>
