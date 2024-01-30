c052306ea1e44ae175930f542a85b599a7366ee8
Vinícius Nascimento <vini_sn@outlook.com>
2024-01-30 08:47:03 -0300

    Formatando os link paginação entre posts

--- wp-content/themes/wp-devs/single.php
+++ wp-content/themes/wp-devs/single.php
@@ -20,10 +20,10 @@
                     <!-- Links de navegação dentro dos posts -->
                     <div class="wpdevs-pagination">
                         <div class="pages next">
-                            <?php next_post_link( "Older posts >>" ); ?>
+                            <?php next_post_link( '&laquo; %link' ); ?>
                         </div>
                         <div class="pages previous">
-                            <?php previous_post_link( "<< Newer posts" ); ?>
+                            <?php previous_post_link( '%link &raquo' ); ?>
                         </div>
                     </div>
 
