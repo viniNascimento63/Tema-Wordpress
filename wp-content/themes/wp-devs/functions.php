72ccd8d6aabb3274a706aec967d923c4e39c8776
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-02 11:13:40 -0300

    Uptdate

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -27,7 +27,8 @@ function wpdevs_config(){
         'flex-height'   => true,
         'flex-width'    => true
     ) );
-
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
+    add_theme_support( 'automatic_feed_links' );
     add_theme_support( 'title-tag' );
 }
 add_action( 'after_setup_theme', 'wpdevs_config', 0 );
@@ -78,4 +79,10 @@ function wpdevs_sidebars(){
             'after_title'   => '</h4>'
         )
     );
+}
+
+if ( !function_exists( 'wp_body_open' ) ) {
+    function wp_body_open() {
+        do_action( 'wp_body_open' );
+    }
 }
