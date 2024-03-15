06aa62e42dc4f64db0d2ba9108bc4d64e8ed96e7
Vinícius Nascimento <vini_sn@outlook.com>
2024-03-15 13:29:02 -0300

    Adicionando suporte customização hero

--- wp-content/themes/wp-devs/inc/customizer.php
+++ wp-content/themes/wp-devs/inc/customizer.php
@@ -29,6 +29,140 @@ function wpdevs_customizer($wp_customize) // Parâmetro é objeto da clase WP_Cu
             'type' => 'text' // tipo campo do controle
         )
     );
+
+    /* SEÇÃO DE HERO */
+    $wp_customize->add_section(
+        'sect_hero', // identificador (id) da seção 
+        array(
+            'title' => 'Hero Settings', // título da seção
+        )
+    );
+
+    // Campo para customizar o título
+    $wp_customize->add_setting(
+        'sett_hero_title', // identificador (id) da setting 
+        array(
+            'type' => 'theme_mod', // tipo de campo a ser guardado no BD
+            'default' => 'Please, add some title here', // Valor padrão dentro do formulário do customizer.
+            'sanitize_callback' => 'sanitize_text_field' // removes all HTML markup, as well as extra whitespace, leaves nothing but plain text
+        )
+    );
+
+    // Controle do title
+    $wp_customize->add_control(
+        'sett_hero_title', // referência à setting que receberá o valor do controle
+        array(
+            'label' => 'Hero Title',
+            'description' => 'Please, add some title',
+            'section' => 'sect_hero', // referência à seção ao qual o controle está ligado
+            'type' => 'text' // tipo campo do controle
+        )
+    );
+
+    // Campo para customizar o subtítulo
+    $wp_customize->add_setting(
+        'sett_hero_subtitle', // identificador (id) da setting 
+        array(
+            'type' => 'theme_mod', // tipo de campo a ser guardado no BD
+            'default' => 'Please, add some subtitle here', // Valor padrão dentro do formulário do customizer.
+            'sanitize_callback' => 'sanitize_textarea_field' // removes all HTML markup, as well as extra whitespace, leaves nothing but plain text
+        )
+    );
+
+    // Controle do subtítulo
+    $wp_customize->add_control(
+        'sett_hero_subtitle', // referência à setting que receberá o valor do controle
+        array(
+            'label' => 'Hero Subtitle',
+            'description' => 'Please, add some subtitle',
+            'section' => 'sect_hero', // referência à seção ao qual o controle está ligado
+            'type' => 'textarea' // tipo campo do controle
+        )
+    );
+
+    // Button Text 
+    $wp_customize->add_setting(
+        'sett_hero_button_text', // identificador (id) da setting 
+        array(
+            'type' => 'theme_mod', // tipo de campo a ser guardado no BD
+            'default' => 'Learn more', // Valor padrão dentro do formulário do customizer.
+            'sanitize_callback' => 'sanitize_textarea_field' // removes all HTML markup, as well as extra whitespace, leaves nothing but plain text
+        )
+    );
+
+    // Button text control
+    $wp_customize->add_control(
+        'sett_hero_button_text', // referência à setting que receberá o valor do controle
+        array(
+            'label' => 'Hero button text',
+            'description' => 'Please, type your hero button text here',
+            'section' => 'sect_hero', // referência à seção ao qual o controle está ligado
+            'type' => 'text' // tipo campo do controle
+        )
+    );
+
+    // Button link 
+    $wp_customize->add_setting(
+        'sett_hero_button_link', // identificador (id) da setting 
+        array(
+            'type' => 'theme_mod', // tipo de campo a ser guardado no BD
+            'default' => '#', // Valor padrão dentro do formulário do customizer.
+            'sanitize_callback' => 'esc_url_raw' // removes all HTML markup, as well as extra whitespace, leaves nothing but plain text
+        )
+    );
+
+    // Button link control
+    $wp_customize->add_control(
+        'sett_hero_button_link', // referência à setting que receberá o valor do controle
+        array(
+            'label' => 'Hero button link',
+            'description' => 'Please, type your hero button link here',
+            'section' => 'sect_hero', // referência à seção ao qual o controle está ligado
+            'type' => 'url' // tipo campo do controle
+        )
+    );
+
+    // Hero height setting
+    $wp_customize->add_setting(
+        'sett_hero_height', // identificador (id) da setting 
+        array(
+            'type' => 'theme_mod', // tipo de campo a ser guardado no BD
+            'default' => 800, // Valor padrão dentro do formulário do customizer.
+            'sanitize_callback' => 'absint' // removes all HTML markup, as well as extra whitespace, leaves nothing but plain text
+        )
+    );
+
+    // Hero height control
+    $wp_customize->add_control(
+        'sett_hero_height', // referência à setting que receberá o valor do controle
+        array(
+            'label' => 'Hero height',
+            'description' => 'Please, type your hero height',
+            'section' => 'sect_hero', // referência à seção ao qual o controle está ligado
+            'type' => 'number' // tipo campo do controle
+        )
+    );
+
+    // Hero background
+    $wp_customize->add_setting(
+        'sett_hero_background', // identificador (id) da setting 
+        array(
+            'type' => 'theme_mod', // tipo de campo a ser guardado no BD
+            'sanitize_callback' => 'absint' // removes all HTML markup, as well as extra whitespace, leaves nothing but plain text
+        )
+    );
+
+    $wp_customize->add_control(
+        new WP_Customize_Media_Control(
+            $wp_customize,
+            'sett_hero_background',
+            array(
+                'label' => 'Hero Image',
+                'section'   => 'sect_hero',
+                'mime_type' => 'image'
+            )
+        )
+    );
 }
 
 // Adicionando a função no gancho
--- wp-content/themes/wp-devs/page-home.php
+++ wp-content/themes/wp-devs/page-home.php
@@ -3,9 +3,26 @@
 <div id="content" class="site-content">
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
+            <?php
+            $hero_title = get_theme_mod('sett_hero_title', 'Please, add some title here');
+            $hero_subtitle = get_theme_mod('sett_hero_subtitle', 'Please, add some subtitle here');
+            $hero_button_text = get_theme_mod('sett_hero_button_text', 'Learn more');
+            $hero_button_link = get_theme_mod('sett_hero_button_link', '#');
+            $hero_height = get_theme_mod('sett_hero_height', 800);
+            $hero_background = wp_get_attachment_url(get_theme_mod('sett_hero_background'));
+            ?>
             <!-- HERO -->
-            <section class="hero">
-                Hero
+            <section class="hero" style="background-image: url('<?php echo $hero_background; ?>')">
+                <!-- Cobertura da imagem -->
+                <div class="overlay" style="min-height: <?php echo $hero_height; ?>px;">
+                    <div class="container">
+                        <div class="hero-items">
+                            <h1><?php echo $hero_title; ?></h1>
+                            <p><?php echo nl2br($hero_subtitle); ?></p>
+                            <a href="<?php echo $hero_button_link ?>"><?php echo $hero_button_text; ?></a>
+                        </div>
+                    </div>
+                </div>
             </section>
 
             <!-- SERVICES -->
